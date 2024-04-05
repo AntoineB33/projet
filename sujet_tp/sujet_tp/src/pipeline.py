3e0cdd511f46a69f13631e1b823b85f864559829
AntoineB33 <86301728+AntoineB33@users.noreply.github.com>
2024-04-05 14:43:10 +0200

    Refactor pipeline.py: Add label encoding and update parameters

--- sujet_tp/sujet_tp/src/pipeline.py
+++ sujet_tp/sujet_tp/src/pipeline.py
@@ -34,7 +34,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import skimage as skimage
-%matplotlib inline
+
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from skimage.feature import hog
@@ -43,18 +43,6 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.cluster import MeanShift, estimate_bandwidth, KMeans
 from sklearn.neural_network import BernoulliRBM
 
-
-params = {
-    'objective': 'multi:softmax', 
-    'num_class': len(np.unique(labels_encoded)),
-    'eta': 0.1,  # Decrease learning rate [0.01-0.3] Lower values make the model more robust but require more boosting rounds.
-    'max_depth': 8,  # Adjust max depth of trees [3-10] Higher values allow the model to capture more complex interactions in the data but can lead to overfitting.
-    'num_boost_round': 100,  # Increase number of boosting rounds [50-500]
-    'subsample': 0.75,  # Reduce subsample ratio [0.5-1] Lower values introduce more randomness and can prevent overfitting.
-    'colsample_bytree': 0.85,  # Reduce column subsample ratio [0.5-1] Lower values introduce more randomness in feature selection.
-    'lambda': 10,  # L2 regularization term [0-10+] Higher values add more regularization to prevent overfitting.
-    'alpha': 10  # L1 regularization term [0-10+] Higher values add more regularization to prevent overfitting.
-}
 
 descsTitle0 = ["RGB", "HSV"]
 descsTitle = ["HISTOGRAM", "HOG", "SIFT"]
@@ -67,6 +55,20 @@ def pipeline():
     images, labels_true, folder_names = load_images_from_folder(folder_path)
     taille = len(images)
     nombre_de_canaux = 3
+    
+    label_encoder = LabelEncoder()
+    labels_encoded = label_encoder.fit_transform(labels_true)
+    params = {
+        'objective': 'multi:softmax', 
+        'num_class': len(np.unique(labels_encoded)),
+        'eta': 0.1,  # Decrease learning rate [0.01-0.3] Lower values make the model more robust but require more boosting rounds.
+        'max_depth': 8,  # Adjust max depth of trees [3-10] Higher values allow the model to capture more complex interactions in the data but can lead to overfitting.
+        'num_boost_round': 100,  # Increase number of boosting rounds [50-500]
+        'subsample': 0.75,  # Reduce subsample ratio [0.5-1] Lower values introduce more randomness and can prevent overfitting.
+        'colsample_bytree': 0.85,  # Reduce column subsample ratio [0.5-1] Lower values introduce more randomness in feature selection.
+        'lambda': 10,  # L2 regularization term [0-10+] Higher values add more regularization to prevent overfitting.
+        'alpha': 10  # L1 regularization term [0-10+] Higher values add more regularization to prevent overfitting.
+    }
    
     print("\n\n ##### Extraction de Features ######")
     
@@ -141,15 +143,32 @@ def pipeline():
             df = create_df_to_export(x_3d_norm, labels_true, preds)
 
             # sauvegarde des données
-            df.to_excel(PATH_OUTPUT+f"/save_clustering_{descs0[d0]}_{descs[d]}_rbm_kmeans.xlsx")
-            print(f"save_clustering_{descs0[d0]}_{descs[d]}_rbm_kmeans.xlsx")
+            df.to_excel(PATH_OUTPUT+f"/save_clustering_{descs0[d0]}_{descs[d]}_xgboost.xlsx")
+            print(f"save_clustering_{descs0[d0]}_{descs[d]}_xgboost.xlsx")
             
             
             
-            
+            scaler = StandardScaler()
+            X_train_normalized = scaler.fit_transform(X_train)
+            X_test_normalized = scaler.fit_transform(X_test)
             bandwidth = estimate_bandwidth(X_train_normalized, quantile=0.06)
             mean_shift = MeanShift(bandwidth=bandwidth,cluster_all=True)
             mean_shift.fit(X_train_normalized)
+            cluster_centers = mean_shift.cluster_centers_
+            metric = show_metric(labels_true, clusters, transformed_images_scaled, bool_show=True, name_descriptor=f"{descsTitle0[d0]} et {descsTitle[d]}", name_model = "Mean Shift", bool_return=True)
+            list_dict.append(metric)
+            
+            
+            scaler = StandardScaler()
+            descriptors_norm = scaler.fit_transform(descriptors[d])
+            
+            x_3d_norm = conversion_3d(descriptors_norm)
+            
+            df = create_df_to_export(x_3d_norm, labels_true, preds)
+
+            # sauvegarde des données
+            df.to_excel(PATH_OUTPUT+f"/save_clustering_{descs0[d0]}_{descs[d]}_mean_shift.xlsx")
+            print(f"save_clustering_{descs0[d0]}_{descs[d]}_mean_shift.xlsx")
             
     df_metric = pd.DataFrame(list_dict)
     df_metric.to_excel(PATH_OUTPUT+"/save_metric.xlsx")
